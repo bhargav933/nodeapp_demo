d926a558e268b824a1452fd8af25cf2f20746387
Bhargav Lalaji <bhargav933@gmail.com>
2022-12-05 16:37:04 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello from DevOps. ');
+  res.send('Hello from DevOps Project. ');
 });
 
 app.listen(PORT, HOST, () => {
